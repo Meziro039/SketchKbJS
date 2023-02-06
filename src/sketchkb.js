7de6d29d0a9f299f4599b44c4e4146296e1c6f6d
Meziro <78205092+Meziro039@users.noreply.github.com>
2023-02-06 19:27:51 +0900

    Ver0.0.1β

--- src/sketchkb.js
+++ src/sketchkb.js
@@ -1,5 +1,5 @@
 /*
-Version: 0.0.0β
+Version: 0.0.1β
 
 License: MIT License
 Copyright: Meziro(https://github.com/Meziro039/)
@@ -16,47 +16,91 @@ class SketchKb {
         history.pushState(null, "", Root + PageId);
         try {
             exit();
-        } catch (e) {}
+        } catch (e) {};
         this.Deploy(PageId);
     };
 
     async Deploy(PageId) {
         const PageKeys = Object.keys(this.Pages[PageId]);
 
+        // Display content processing.
         for (let i = 0;i < PageKeys.length;i++) {
             if (PageKeys[i].match(/^#/)) {
                 let DrawingHtml = [];
-                const Id = PageKeys[i].replace(/^#/, "")
-                const Content = document.getElementById(Id) ? true : false;
+                const ElementId = PageKeys[i].replace(/^#/, "");
+                const Content = document.getElementById(ElementId) ? true : false;
+
                 if (Content) {
-                    const IdData = this.Pages[PageId]["#" + Id];
-                    const IdKey = Object.keys(IdData);
-                    if (IdKey.includes("Html")) {
-                        for (let j = 0;j < IdData["Html"].length;j++) {
-                            const MainData = IdData["Html"][j];
-                            if (typeof MainData === "string") {
+                    console.log(this.Pages[PageId]);
+                    const ContentData = this.Pages[PageId]["#" + ElementId];
+                    const ContentKey = Object.keys(ContentData);
+                    if (ContentKey.includes("Html")) {
+                        for (let j = 0;j < ContentData["Html"].length;j++) {
+                            const CodePath = ContentData["Html"][j];
+                            if (typeof CodePath === "string") {
                                 // Code
-                                DrawingHtml.push(MainData);
+                                DrawingHtml.push(CodePath);
                             } else {
                                 // Path
-                                for (let k = 0;k < MainData.length;k++) {
-                                    DrawingHtml.push(await (await fetch(MainData[k])).text());
+
+                                // Cache check.
+                                let CacheData = "";
+                                const CacheCheck = sessionStorage.getItem(CodePath) ? true : false;
+                                if (CacheCheck) {
+                                    CacheData = sessionStorage.getItem(CodePath);
+                                } else {
+                                    try {
+                                        sessionStorage.setItem(CodePath, await (await fetch(CodePath)).text());
+                                        CacheData = sessionStorage.getItem(CodePath);
+                                    } catch (e) {
+                                        CacheData = await (await fetch(CodePath)).text();
+                                    };
                                 };
+
+                                DrawingHtml.push(CacheData);
                             };
                         };
                     };
-                    if (IdKey.includes("Css")) {
-                        for (let j = 0;j < IdData["Css"].length;j++) {
-                            const MainData = IdData["Css"][j];
-                            // Path 仮
-                            DrawingHtml.push("<link rel=\"stylesheet\" href=\"" + MainData + "\">");
+                    if (ContentKey.includes("Css")) {
+                        for (let j = 0;j < ContentData["Css"].length;j++) {
+                            const Path = ContentData["Css"][j];
+
+                            // Cache check.
+                            let CacheData = "";
+                            const CacheCheck = sessionStorage.getItem(Path) ? true : false;
+                            if (CacheCheck) {
+                                CacheData = sessionStorage.getItem(Path);
+                            } else {
+                                try {
+                                    sessionStorage.setItem(Path, await (await fetch(Path)).text());
+                                    CacheData = sessionStorage.getItem(Path);
+                                } catch (e) {
+                                    CacheData = await (await fetch(Path)).text();
+                                };
+                            };
+
+                            DrawingHtml.push("<style>" + CacheData + "</style>");
                         };
                     };
-                    if (IdKey.includes("Js")) {
-                        for (let j = 0;j < IdData["Js"].length;j++) {
-                            const MainData = IdData["Js"][j];
-                            // Path 仮
-                            DrawingHtml.push("<script src=\"" + MainData + "\"></script>");
+                    if (ContentKey.includes("Js")) {
+                        for (let j = 0;j < ContentData["Js"].length;j++) {
+                            const Path = ContentData["Js"][j];
+
+                            // Cache check.
+                            let CacheData = "";
+                            const CacheCheck = sessionStorage.getItem(Path) ? true : false;
+                            if (CacheCheck) {
+                                CacheData = sessionStorage.getItem(Path);
+                            } else {
+                                try {
+                                    sessionStorage.setItem(Path, await (await fetch(Path)).text());
+                                    CacheData = sessionStorage.getItem(Path);
+                                } catch (e) {
+                                    CacheData = await (await fetch(Path)).text();
+                                };
+                            };
+
+                            DrawingHtml.push("<script>" + CacheData + "</script>");
                         };
                     };
 
@@ -64,92 +108,50 @@ class SketchKb {
 
                     // Javascript conv.
                     let JsArray = [];
-                    const JsCheckTemp = DrawingHtml.replace(/<!--.*?-->/gs, "").match(/<script.*?>.*?<\/script>/gs);
-                    if (JsCheckTemp != null && JsCheckTemp.length !== 0) {
-                        for (let j = 0;j < JsCheckTemp.length;j++) {
-                            DrawingHtml = DrawingHtml.replaceAll(JsCheckTemp[j], "");
+                    const JsCheck = DrawingHtml.replace(/<!--.*?-->/gs, "").match(/<script.*?>.*?<\/script>/gs);
+                    if (JsCheck != null && JsCheck.length !== 0) {
+                        for (let j = 0;j < JsCheck.length;j++) {
+                            DrawingHtml = DrawingHtml.replaceAll(JsCheck[j], "");
                             const NewElement = document.createElement("script");
-                            const ScriptSuevey = (new DOMParser()).parseFromString(JsCheckTemp[j], "text/html").scripts[0];
+                            const ScriptSuevey = (new DOMParser()).parseFromString(JsCheck[j], "text/html").scripts[0];
                             if (ScriptSuevey.src != "") {
                                 NewElement.src = ScriptSuevey.src;
                             };
                             if (ScriptSuevey.type != "") {
                                 NewElement.type = ScriptSuevey.type;
-                            };
-                            
+                            };                            
                             if (ScriptSuevey.crossOrigin != null) {
                                 NewElement.crossOrigin = ScriptSuevey.crossOrigin;
-                            };
-                            
+                            };                            
                             if (ScriptSuevey.integrity != "") {
                                 NewElement.integrity = ScriptSuevey.integrity;
-                            };
-                            
+                            };                           
                             if (ScriptSuevey.referrerPolicy != "") {
                                 NewElement.referrerPolicy = ScriptSuevey.referrerPolicy;
-                            };
-                            
+                            };                           
                             if (ScriptSuevey.defer != false) {
                                 NewElement.defer = ScriptSuevey.defer;
-                            };
-                            
+                            };                            
                             if (ScriptSuevey.async != false) {
                                 NewElement.async = ScriptSuevey.async;
-                            };
-                            
+                            }; 
                             if (ScriptSuevey.textContent != "") {
                                 NewElement.textContent = ScriptSuevey.textContent;
                             };
                             JsArray.push(NewElement);
                         };
                     };
-                    document.getElementById(Id).innerHTML = DrawingHtml;
-                    document.getElementById(Id).appendChild(JsArray[0])
+
+                    // Drawing
+                    document.getElementById(ElementId).innerHTML = DrawingHtml;
+                    for (let j = 0;j < JsArray.length;j++) {
+                        document.getElementById(ElementId).appendChild(JsArray[j]);
+                    };
                 };
             };
         };
     };
 };
-
-/*
-class SketchKb {
-    constructor(Pages) {
-        this.Pages = Pages;
-        this.Before = "";
-        console.log(this.Pages);
-    };
-
-    Deploy(Path) {
-        let Pg = this.Pages;
-        let Bf = this.Before;
-
-        // 一致データPASS
-        for (let i = 0;i < Pg["Data"]["Html"].length;i++) {
-
-        };
-
-        this.Before = Path;
-    };
-
-    Route(Path, Root) {
-        Root = Root ? Root : "";
-        history.pushState(null, "", Root + Path);
-        this.Deploy(Path);
-    };
-
-    Html(Input, IsCode) {
-
-    };
-
-    Css(Input) {
-
-    };
-
-    Js(Input) {
-
-    };
-};
-*/
 
 console.log("Load: SketchKbJS");
 console.log("%cSTOP!!\nIf you don't know how to use it, don't touch it.","font-size:20px");
